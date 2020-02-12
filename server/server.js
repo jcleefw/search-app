9c1bef73de7de7248bf31a6061aebd4053a30547
JC Lee <jcleefw@gmail.com>
2020-02-13 07:01:50 +1100

    created a express server to server have an endpoint for search results

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+var app = express()
+
+const searchResults = require('./data.json')
+
+app.set('port', process.env.PORT || 8080)
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/search_results', (req, res) => res.json(searchResults))
+
+app.listen(app.get('port'), () =>
+  console.log(`Example app listening on port ${app.get('port')}!`)
+)
