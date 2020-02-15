5e6b52d900f7b3ade3fbe53fd70f055f6b412945
JC Lee <jcleefw@gmail.com>
2020-02-15 13:41:39 +1100

    update readme

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var app = express()
 
 const searchResults = require('./data.json')
 
-app.set('port', process.env.PORT || 8080)
+app.set('port', process.env.PORT || 5000)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
