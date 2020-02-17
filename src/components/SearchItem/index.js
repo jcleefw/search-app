cf5d2d978b4ecec5d48c0a471e2abb577b38e205
JC Lee <jia.chee@greensync.com.au>
2020-02-17 20:29:09 +1100

    added all need details on page

--- src/components/SearchItem/index.js
+++ src/components/SearchItem/index.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import './search-item.scss'
 
-const renderItemPrice = propertyOffer => {
-  return (
-    <div className="pricePerNight">{propertyOffer.displayPrice.amount}</div>
-  )
-}
-
 const renderItemImage = (imgDetails, promotionTitle) => {
   return (
     <figure className="itemFigure">
       <img src={imgDetails.url} alt={imgDetails.caption} />
       <span>{promotionTitle}</span>
     </figure>
+  )
+}
+
+const renderItemDetails = (propertyDetails, offerName, cancellationType) => {
+  const freeCancellation =
+    cancellationType === 'FREE_CANCELLATION' ? true : false
+  return (
+    <div className="propertyDetails">
+      <h4 className="propertyName">{propertyDetails.title}</h4>
+      <p className="propertyAddress">{propertyDetails.address.join()}</p>
+      <p className="propertyType">
+        <a href="#">{offerName}</a>
+      </p>
+      {freeCancellation && (
+        <p className="freeCancellation">Free Cancellation</p>
+      )}
+    </div>
+  )
+}
+
+const renderItemPrice = propertyOffer => {
+  const propertySavings = propertyOffer.savings
+    ? propertyOffer.savings.amount
+    : null
+  return (
+    <div className="propertyPriceDetails">
+      <p className="caption">
+        <span>1</span> night total{' '}
+        {propertyOffer.displayPrice.currency.toUpperCase()}
+      </p>
+      <div className="pricePerNight">{propertyOffer.displayPrice.amount}</div>
+      {propertySavings && <div className="savings">${propertySavings}</div>}
+    </div>
   )
 }
 
@@ -23,7 +50,11 @@ const SearchItem = ({ index, details }) => {
         details.property.previewImage,
         details.offer.promotion.title
       )}
-      <p>{details.property.title}</p>
+      {renderItemDetails(
+        details.property,
+        details.offer.name,
+        details.offer.cancellationOption.cancellationType
+      )}
       {renderItemPrice(details.offer)}
     </article>
   )
