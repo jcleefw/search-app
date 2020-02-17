ad676b27c899ec919fb4aa0df0c0eef58271e439
JC Lee <jia.chee@greensync.com.au>
2020-02-17 12:29:24 +1100

    return property title if search results is not empty

--- src/components/SearchContainer/SearchList.jsx
+++ src/components/SearchContainer/SearchList.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './search-page-styles.scss'
 
 const SearchList = ({ searchResults, searchQuery }) => {
+  const isEmptyList = searchResults.length > 0 ? false : true
+  console.log(searchResults)
   return (
     <div className="searchList" data-testid="searchList">
       <div>
         {searchResults.length} hotels in {searchQuery}
       </div>
+
+      {!isEmptyList && searchResults.map((item, index) =>
+        <div>{item.property.title}</div>
+      )}
     </div>
   )
 }
