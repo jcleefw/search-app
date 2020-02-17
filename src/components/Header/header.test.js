4f120dafbc8ab150cb5a72ee6116d228deb2a8cf
JC Lee <jia.chee@greensync.com.au>
2020-02-17 11:52:11 +1100

    add test for header

---
+++ src/components/Header/header.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Header from '.'
+
+describe('Header', () => {
+  it('renders the Header', () => {
+    const wrapper = shallow(<Header />)
+    expect(wrapper.find('.header')).toHaveLength(1)
+    expect(wrapper.find('.headerLogo')).toHaveLength(1)
+  })
+})
